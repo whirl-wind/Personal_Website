c70f36d1c0d96b74115ccd09834423e40ecca260
Zhan Zhang <whirl-wind@mail.ustc.edu.cn>
2020-02-10 11:01:47 +0800

    111

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-import '../sass/main.scss';
-
-import './testJquery.js';
-import './toggle.js';
-import './fadeEffect.js';
-import './switchTabs.js';
-import './jump.js';
-import './scrollbar.js';
--- src/postIndex.js
+++
@@ -1,7 +0,0 @@
-import _ from 'lodash';
-import '../sass/md.scss';
-
-import './testJquery.js';
-import './scrollbar.js';
-import './toggle.js';
-import './externalLink.js';
